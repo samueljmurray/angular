72540224d26669d39075e59adaec49209a7f4cec
Sam Murray <SamM@arnold.local>
2014-11-03 15:08:16 +0000

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.when('', '/');
+
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .state('404', {
+        url: '{path:.*}',
+        template: '<div>NOT FOUND!</div>'
+      });
+
+  });
